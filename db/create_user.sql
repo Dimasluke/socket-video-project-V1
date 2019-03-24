f20ecd2585ffd5994af735092c2c519d8f75a361
Luke Dimas <dimasluke@gmail.com>
2019-03-24 13:32:35 -0700

    userController complete with postman testing"

---
+++ db/create_user.sql
@@ -0,0 +1,2 @@
+insert into users (firstName, lastName, email, username, password)
+values($1, $2, $3, $4, $5);
--- server/index.js
+++ server/index.js
@@ -1,1 +1,45 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const socket = require('socket.io')
+const massive = require('massive')
+const session = require('express-session')
+const userController = require('./controllers/UserController')
+
+app.use(bodyParser.json())
+require('dotenv').config()
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 60 * 60 * 1000 * 24 * 14
+        }
+    })
+)
+
+massive(process.env.CONNECTION_STRING)
+    .then(db => {
+        console.log('Connected to database.')
+        app.set('db', db)
+        db.init()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.get('/api/sessionInfo', userController.sessionInfo)
+app.post('/api/register', userController.register)
+app.post('/api/login', userController.login)
+app.post('/api/logout', userController.logout)
+
+const port = process.env.PORT || 4000
+const io = socket(app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+}))
+
+io.on('connection', socket => {
+    console.log('User Connected')
+})
