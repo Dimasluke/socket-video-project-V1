5fde6a8b0884a73e2b4effddd25aad506025b3c6
Richard Bond <richardbond@Richards-MacBook-Pro.local>
2019-03-26 15:47:04 -0700

    videos now come from state

--- src/Components/VideoRoom/VideoPlayer/VideoPlayer.js
+++ src/Components/VideoRoom/VideoPlayer/VideoPlayer.js
@@ -1,42 +1,45 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom'
-import './VideoPlayer.css'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import "./VideoPlayer.css";
 
 class VideoPlayer extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            selectedRoom: {}
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedRoom: {}
+    };
+  }
 
-    componentDidMount () {
-        const selectedRoom = this.props.rooms.filter(room => {
-            return room.id == this.props.match.params.roomId
-        })
-        this.setState({
-            selectedRoom: selectedRoom[0]
-        })
-    }
+  componentDidMount() {
+    const selectedRoom = this.props.rooms.filter(room => {
+      return room.id == this.props.match.params.roomId;
+    });
+    this.setState({
+      selectedRoom: selectedRoom[0]
+    });
+  }
 
-    render(){
-        return (
-            <div className='video-component'>
-                <iframe src='https://www.youtube.com/embed/HZFTUtbn1RU' className='container video-container' ></iframe>
-                <div>
-                    {this.state.selectedRoom.owner}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    let { videoUrl } = this.state.selectedRoom;
+    console.log(videoUrl);
+    return (
+      <div className="video-component">
+        <iframe src={videoUrl} className="container video-container" />
+        <div>{this.state.selectedRoom.owner}</div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        user: state.user.username,
-        rooms: state.room.rooms
-    }
-}
+  return {
+    user: state.user.username,
+    rooms: state.room.rooms
+  };
+};
 
-export default connect(mapStateToProps, null)(withRouter(VideoPlayer))
+export default connect(
+  mapStateToProps,
+  null
+)(withRouter(VideoPlayer));
