ed2dfda44bad252f6b3076a34c0f77712642543c
Richard Bond <richardbond@Richards-MacBook-Pro.local>
2019-03-26 09:47:21 -0700

    added somethign random

--- src/Redux/Reducers/RoomReducer.js
+++ src/Redux/Reducers/RoomReducer.js
@@ -1,27 +1,28 @@
 const initialState = {
-    rooms: [
-        {
-            id: 1,
-            roomName:'room 1',
-            description: 'happy happy joy joy', 
-            owner: 'Daniel',
-            videoUrl: 'https://youtu.be/HZFTUtbn1RU'
-        },
-        {
-            id: 2,
-            roomName:'room2',
-            description: 'cats', 
-            owner: 'Bryce',
-            videoUrl: 'https://youtu.be/XJYmyYzuTa8'
-        }
-    ]
-}
-
-function RoomReducer (state = initialState, action){
-    switch(action.type){
-        default:
-            return state;
+  //somthing random
+  rooms: [
+    {
+      id: 1,
+      roomName: "room 1",
+      description: "happy happy joy joy",
+      owner: "Daniel",
+      videoUrl: "https://youtu.be/HZFTUtbn1RU"
+    },
+    {
+      id: 2,
+      roomName: "room2",
+      description: "cats",
+      owner: "Bryce",
+      videoUrl: "https://youtu.be/XJYmyYzuTa8"
     }
+  ]
+};
+
+function RoomReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
 
-export default RoomReducer
+export default RoomReducer;
