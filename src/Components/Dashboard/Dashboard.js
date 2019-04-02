578aa5eb4317e766bff78b1d4e19e4b978a5f7a7
Richard Bond <richardbond@Richards-MacBook-Pro.local>
2019-04-02 11:06:46 -0700

    responsive dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,53 +1,56 @@
-import React, { Component } from 'react';
-import Navbar from '../Navbar/Navbar';
-import RoomCard from '../RoomCard/RoomCard';
-import { createRoom, setRooms } from '../../Redux/Reducers/RoomReducer'
-import { connect } from 'react-redux'
-import './Dashboard.css'
-import axios from 'axios';
+import React, { Component } from "react";
+import Navbar from "../Navbar/Navbar";
+import RoomCard from "../RoomCard/RoomCard";
+import { createRoom, setRooms } from "../../Redux/Reducers/RoomReducer";
+import { connect } from "react-redux";
+import "./Dashboard.css";
+import axios from "axios";
 
 class Dashboard extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            rooms: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: []
+    };
+  }
 
-    componentDidMount(){
-        axios.get('/api/rooms').then(response => {
-            console.log(response.data)
-            this.props.setRooms(response.data)
-        })
-    }
+  componentDidMount() {
+    axios.get("/api/rooms").then(response => {
+      console.log(response.data);
+      this.props.setRooms(response.data);
+    });
+  }
 
-    render(){
-        const mappedRooms = this.props.rooms.map((room, index) => {
-            return (
-                <div key={index} className='dashboard-room-card'>
-                    <RoomCard 
-                        roomName={room.title || room.roomName}
-                        owner={room.owner}
-                        description={room.description}
-                        id={room.id}/> 
-                </div>
-            )
-        })
-        return(
-            <div>
-                <Navbar />
-                <div className='container room-card-container'>
-                    {mappedRooms}        
-                </div>
-            </div>
-        )
-    }
+  render() {
+    let reversedRooms = this.props.rooms.reverse();
+    const mappedRooms = reversedRooms.map((room, index) => {
+      return (
+        <div key={index} className="dashboard-room-card">
+          <RoomCard
+            roomName={room.title || room.roomName}
+            owner={room.owner}
+            description={room.description}
+            id={room.id}
+          />
+        </div>
+      );
+    });
+    return (
+      <div>
+        <Navbar />
+        <div className="container room-card-container">{mappedRooms}</div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        rooms: state.room.rooms
-    }
-}
+  return {
+    rooms: state.room.rooms
+  };
+};
 
-export default connect(mapStateToProps, {createRoom, setRooms})(Dashboard)
+export default connect(
+  mapStateToProps,
+  { createRoom, setRooms }
+)(Dashboard);
--- src/Components/RoomCard/RoomCard.js
+++ src/Components/RoomCard/RoomCard.js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class RoomCard extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        }
-    }
-
-    render(){
-        const { roomName, owner, description, id } = this.props
-        return(
-            <div className='card dashboard-cards' style={{width: '20rem'}}>
-                <div className='card-body'>
-                    <h5 className='card-title'>{roomName}</h5>
-                    <h6 className='card-subtitle mb-2 text-muted'>{owner}</h6>
-                    <p className='card-text'>{description}</p>
-                    <Link to={`/${id}`}>Join Room</Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { roomName, owner, description, id } = this.props;
+    return (
+      <div
+        id="responsive-card"
+        className="card dashboard-cards"
+        style={{ width: "20rem" }}
+      >
+        <div className="card-body">
+          <h5 className="card-title">{roomName}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{owner}</h6>
+          <p className="card-text">{description}</p>
+          <Link to={`/${id}`}>Join Room</Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        user: state.user.username,
-        room: state.room.rooms
-    }
-}
+  return {
+    user: state.user.username,
+    room: state.room.rooms
+  };
+};
 
-export default connect(mapStateToProps, null)(RoomCard)
+export default connect(
+  mapStateToProps,
+  null
+)(RoomCard);
